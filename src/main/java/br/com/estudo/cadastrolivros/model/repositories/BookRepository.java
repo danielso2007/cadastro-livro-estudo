45809b9b1191945f6ebf2d12dc1739f76876ec19
danielso2007 <danielso2007@gmail.com>
2016-09-25 18:47:23 -0300

    Finalizando para a versão 1.0.0. Criado a pesquisa de livros por título e autor.

--- src/main/java/br/com/estudo/cadastrolivros/model/repositories/BookRepository.java
+++ src/main/java/br/com/estudo/cadastrolivros/model/repositories/BookRepository.java
@@ -1,7 +1,9 @@
 package br.com.estudo.cadastrolivros.model.repositories;
 
 import br.com.estudo.cadastrolivros.model.domain.Book;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +11,7 @@ import java.util.List;
 @Repository
 public interface BookRepository extends CrudRepository<Book, Long> {
 
-    List<Book> search(String description);
+    @Query("SELECT b FROM Book b WHERE b.status = 'PUBLISHED' AND (lower(b.author) LIKE lower('%' || :description || '%') OR lower(b.title) LIKE lower('%' || :description || '%')) ORDER BY b.title")
+    List<Book> search(@Param("description") String description);
 
 }
