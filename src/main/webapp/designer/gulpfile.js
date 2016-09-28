f9eda1a20bd443b509ddfada76b32b995ef909cc
danielso2007 <danielso2007@gmail.com>
2016-09-28 00:08:26 -0300

    Ajustando estrutura para o MVC.

--- src/main/webapp/designer/gulpfile.js
+++ src/main/webapp/designer/gulpfile.js
@@ -9,29 +9,29 @@ var runSequence = require('run-sequence');
 var inject = require('gulp-inject');
 
 var yeoman = {
-  app: require('./bower.json').appPath || '../app',
-  dist: 'dist'
+    app: require('./bower.json').appPath || '../app',
+    dist: 'dist'
 };
 
 var paths = {
-  scripts: [yeoman.app + '/restrict/scripts/**/*.js', yeoman.app + '/public/scripts/**/*.js'],
-  styles: [yeoman.app + '/styles/**/*.css'],
-  test: ['test/spec/**/*.js'],
-  testRequire: [
-    'bower_components/angular/angular.js',
-    'bower_components/angular-mocks/angular-mocks.js',
-    'bower_components/angular-resource/angular-resource.js',
-    'bower_components/angular-cookies/angular-cookies.js',
-    'bower_components/angular-sanitize/angular-sanitize.js',
-    'bower_components/angular-route/angular-route.js',
-    'test/mock/**/*.js',
-    'test/spec/**/*.js'
-  ],
-  karma: 'karma.conf.js',
-  views: {
-    main: yeoman.app + '/restrict/index.html',
-    files: [yeoman.app + '/restrict/views/**/*.html']
-  }
+    scripts: [yeoman.app + '/restrict/scripts/**/*.js', yeoman.app + '/public/scripts/**/*.js'],
+    styles: [yeoman.app + '/styles/**/*.css'],
+    test: ['test/spec/**/*.js'],
+    testRequire: [
+        'bower_components/angular/angular.js',
+        'bower_components/angular-mocks/angular-mocks.js',
+        'bower_components/angular-resource/angular-resource.js',
+        'bower_components/angular-cookies/angular-cookies.js',
+        'bower_components/angular-sanitize/angular-sanitize.js',
+        'bower_components/angular-route/angular-route.js',
+        'test/mock/**/*.js',
+        'test/spec/**/*.js'
+    ],
+    karma: 'karma.conf.js',
+    views: {
+        main: yeoman.app + '/restrict/index.html',
+        files: [yeoman.app + '/restrict/views/**/*.html']
+    }
 };
 
 ////////////////////////
@@ -39,54 +39,102 @@ var paths = {
 ////////////////////////
 
 var lintScripts = lazypipe()
-  .pipe($.jshint, '.jshintrc')
-  .pipe($.jshint.reporter, 'jshint-stylish');
+    .pipe($.jshint, '.jshintrc')
+    .pipe($.jshint.reporter, 'jshint-stylish');
 
-gulp.task('lint:scripts', function () {
-  return gulp.src(paths.scripts).pipe(lintScripts());
+gulp.task('lint:scripts', function() {
+    return gulp.src(paths.scripts).pipe(lintScripts());
 });
 
 // inject bower components
-gulp.task('bower', function () {
-  var option = {
-               directory: yeoman.app + '/public/lib',
-               bowerJson: './bower.json',
-               src: [yeoman.app + '/public/lib' + '/font-awesome/css/font-awesome.min.css'],
-               cwd: paths.views.main,
-//               ignorePath: 'public/'
-             };
+gulp.task('bower', function() {
+    var option = {
+        directory: './bower_components',
+        src: ['./bower_components/font-awesome/css/font-awesome.min.css'],
+        ignorePath: '../../designer/bower_components',
+        fileTypes: {
+            html: {
+                block: /(([ \t]*)<!--\s*bower:*(\S*)\s*-->)(\n|\r|.)*?(<!--\s*endbower\s*-->)/gi,
+                detect: {
+                    js: /<script.*src=['"]([^'"]+)/gi,
+                    css: /<link.*href=['"]([^'"]+)/gi
+                },
+                replace: {
+                    js: function(filePath) {
+                        return '<script src="../lib' + filePath + '"></script>';
+                    },
+                    css: function(filePath) {
+                        return '<link rel="stylesheet" type="text/css" href="../lib' + filePath + '"/>';
+                    }
+                }
+            }
+        }
+    };
 
-  return gulp.src(paths.views.main).pipe(wiredep(option)).pipe(gulp.dest(yeoman.app + '/restrict'));
+    return gulp.src(paths.views.main).pipe(wiredep(option)).pipe(gulp.dest(yeoman.app + '/restrict'));
 });
 
-gulp.task('inject', function () {
-  var target = gulp.src(paths.views.main);
-  var sources = gulp.src(['./vendor/**/*min.js', './vendor/**/*min.css', './styles/**/*.css', yeoman.app + '/restrict/scripts/*.js', yeoman.app + '/restrict/scripts/**/*.js'], {read: false});
-  var option = {relative: true};
+gulp.task('inject', function() {
+    var target = gulp.src(paths.views.main);
+    var sources = gulp.src([
+      yeoman.app + 'public/vendor/**/*min.js',
+      yeoman.app + 'public/vendor/**/*min.css',
+      yeoman.app + 'public/styles/*.css',
+      yeoman.app + 'restrict/scripts/*.js',
+      yeoman.app + 'restrict/scripts/**/*.js'
+    ], {read: false});
 
-  return target.pipe(inject(sources, option)).pipe(gulp.dest(yeoman.app + '/restrict'));
+    var option = {relative: true};
+
+    return target.pipe(inject(sources, option)).pipe(gulp.dest(yeoman.app + '/restrict'));
 });
 
 
-gulp.task('images', function () {
-  return gulp.src('./images/**/*')
-    .pipe($.cache($.imagemin({
-        optimizationLevel: 5,
-        progressive: true,
-        interlaced: true
-    })))
-    .pipe(gulp.dest(yeoman.app + '/public/images'));
+gulp.task('images', function() {
+    return gulp.src('./images/**/*')
+        .pipe($.cache($.imagemin({
+            optimizationLevel: 5,
+            progressive: true,
+            interlaced: true
+        })))
+        .pipe(gulp.dest(yeoman.app + '/public/images'));
 });
 
-gulp.task('copy:libs', function () {
-  return gulp.copy(['./bower_components/**/*.css', './bower_components/**/*.js'], yeoman.app + '/public/lib');
+gulp.task('copy:libs', function() {
+    return gulp.copy([
+    './bower_components/**/*.css',
+    './bower_components/**/*.otf',
+    './bower_components/**/*.eot',
+    './bower_components/**/*.svg',
+    './bower_components/**/*.ttf',
+    './bower_components/**/*.woff',
+    './bower_components/**/*.woff2',
+    './bower_components/**/*.js'],
+    yeoman.app + '/public/lib',
+    'bower_components');
 });
 
-gulp.copy=function(src,dest){
-    return gulp.src(src, {base:"bower_components"})
-    .pipe(gulp.dest(dest));
+gulp.task('copy:vendor', function() {
+    return gulp.copy([
+    './vendor/**/*min.*'],
+    yeoman.app + '/public/vendor',
+    '');
+});
+
+gulp.task('copy:styles', function() {
+    return gulp.copy([
+    './styles/*.css'],
+    yeoman.app + '/public/styles',
+    '');
+});
+
+gulp.copy = function(src, dest, base) {
+    return gulp.src(src, {
+            base: base
+        })
+        .pipe(gulp.dest(dest));
 };
 
-gulp.task('default', ['bower'], function () {
-  runSequence(['inject', 'images']);
+gulp.task('default', ['bower'], function() {
+    runSequence(['inject', 'images']);
 });
